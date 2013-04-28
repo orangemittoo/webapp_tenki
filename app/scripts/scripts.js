fdc8388f9a9572e040822cae77a9da78f0f647fc
orangemittoo <maniacsound19@gmail.com>
2013-04-28 11:09:55 +0900

    update

--- app/scripts/scripts.js
+++ app/scripts/scripts.js
@@ -1,42 +1,54 @@
 (function($, window, undefined){
-	var CANVAS_W = 320;
+	var CANVAS_W = 320,
+		isTouch = 'ontouchend' in document,
+		eventName = (function(en){
+			return {
+				start : en[0],
+				move : en[1],
+				end :en[2]
+			};
+		})((isTouch ? 'touchstart touchmove touchend' : 'mousedown mousemove mouseup').split(' '));
 
 	var app = {
 		init : function() {
 			this.layerElm = $('.layer');
 			this.startX = 0;
-			this.currentPos = 0;
+			this.isMove = false;
 			this.currentDeg = 0;
 			this.initEvent();
 		},
 		initEvent: function() {
+			test = 1;
 			var doc = $(window.document)[0];
-			doc.addEventListener('touchstart', this);
-			doc.addEventListener('touchmove', this);
-			doc.addEventListener('touchend', this);
+			doc.addEventListener(eventName.start, this);
+			doc.addEventListener(eventName.move, this);
+			doc.addEventListener(eventName.end, this);
 		},
 		handleEvent: function(event) {
 			switch(event.type){
-				case 'touchstart' : 
+				case eventName.start :
 					this.onTouchStart(event);
 					break;
-				case 'touchmove' : 
+				case eventName.move :
 					this.onTouchMove(event);
 					break;
-				case 'touchend' : 
+				case eventName.end :
 					this.onTouchEnd(event);
 					break;
 			}
 		},
 		onTouchStart: function(e) {
-			this.startX = e.touches[0].clientX;
+			this.isMove = true;
+			this.startX = isTouch ? e.touches[0].clientX : e.clientX;
 		},
 		onTouchMove: function(e) {
-			var moveX = e.touches[0].clientX - this.startX;
+			if(!this.isMove) {return;}
+			var moveX = (isTouch ? e.touches[0].clientX : e.clientX) - this.startX;
 			this.changeTransform(moveX);
 		},
 		onTouchEnd: function(e) {
-			var moveX = e.changedTouches[0].clientX - this.startX;
+			this.isMove = false;
+			var moveX = (isTouch ? e.changedTouches[0].clientX : e.clientX) - this.startX;
 			if(Math.abs(moveX) > 100) {
 				if(moveX < 0) {
 					this.currentDeg -= 90;
@@ -47,13 +59,13 @@
 			this.setTransform(this.currentDeg);
 		},
 		changeTransform: function(distance) {
-			var deg = distance / CANVAS_W * 90 | 0;
+			var deg = (distance / CANVAS_W * 90) | 0;
 			this.setTransform(this.currentDeg + deg);
 		},
 		setTransform : function(deg) {
 			this.layerElm.css('-webkit-transform', 'translate3d(0px, 0px, -160px) rotate3d(0, 1, 0, '+deg+'deg)');
 		}
-	}
+	};
 
 	app.init();
 })(jQuery, window);
